bfcf49fe01a4767e92a96de92e18a271e8513667
VolZa <vvolza@gmail.com>
2023-02-15 08:20:34 +0200

    dz07 edit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,52 +1,78 @@
 import { galleryItems } from './gallery-items.js';
 
-// import * as basicLightbox from 'basiclightbox';
-
 // Change code below this line
-const refs = {
-    galleryEl: document.querySelector('.gallery'),
-    galleryListEl: document.querySelector('.gallery__list'),
-}
+const galleryEl = document.querySelector('.gallery');
+
 let creatGalleryPreview = ""; 
-let creatGalleryOriginal = ""; 
-for (let item of galleryItems) {
-    creatGalleryPreview += 
-    (`<li><img src=${item.preview} alt="${item.description}" width="340"/></li>`)
-}
 
-refs.galleryListEl.insertAdjacentHTML('afterbegin', creatGalleryPreview);
 
-refs.galleryListEl.addEventListener("click", vievFullImage);
+// for (let item of galleryItems) {
+//     creatGalleryPreview +=
+//     (`<li><img style="dissplay:block; object-fit: cover; height=100%;"src=${item.preview} alt="${item.description}" width="340" /></li>`)
+// }
 
-refs.galleryListEl.style.display = 'flex';
-refs.galleryListEl.style.flexWrap = 'wrap';
-refs.galleryListEl.style.gap = '10px';
-refs.galleryListEl.style.listStyle = 'none';
+galleryItems.forEach(item => {
+    creatGalleryPreview +=
+    (`<div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}"
+            style="dissplay:block; object-fit: cover; height=100%;"src=${item.preview} alt="${item.description}" width="340" />
+        </a>
+    </div>`)
+});
+
+// Add code to DOM here
+galleryEl.insertAdjacentHTML('beforeend', creatGalleryPreview);
+
+// Add a listener to a gallery item
+galleryEl.addEventListener("click", vievFullImage);
+
+// galleryEl.style.display = 'flex';
+// galleryEl.style.flexWrap = 'wrap';
+// galleryEl.style.gap = '10px';
+// galleryEl.style.listStyle = 'none';
+// galleryEl.style.cursor = 'zoom-in';
+// galleryEl.style.cursor.borderColor = 'read';
+
+// refs.galleryEl.children[0].children[0].src
+// console.log(`refs.galleryListEl.style = ${refs.galleryEl.children[0].children[0].src}`);
+
+
+
+
 
 function vievFullImage(ev) {
-    // console.log(refs.galleryListEl.children);
-    // console.log(ev.target.parentNode);
-    if (ev.target.hasAttribute('alt')) {
-        const altValue = ev.target.getAttribute("alt");
-        console.log(ev.target);
-        // console.log(altValue);
-        for (const image of galleryItems) {
-            if (image.description === altValue) {
-                const bigImg = ev.target.getAttribute("src");
-                console.log(bigImg);
-                console.log(ev.target);
-                ev.target.setAttribute("src", bigImg);
-                
-                const instance = basicLightbox.create(`
-                <img src="${image.original}" width="1280">
-                `);
-                instance.show();
-                break;
-            }
-        };
+    // Override the default behavior
+    ev.preventDefault();
 
+    // checking whether the click was on the image
+    if (ev.target.nodeName !== "IMG") {
+        return;
     }
     
+
+
+
+    // get the value of the alt attribute  for the clicked element 
+    let altValue = ev.target.getAttribute("alt");
+
+    for (const image of galleryItems) {
+        if (image.description === altValue) {
+            const bigImg = image.original;               
+            
+            const instance = basicLightbox.create(`
+            <img src="${bigImg}" width="1280">
+            `);
+
+            instance.show();
+            function closeModal(ev) {
+                if (ev.code === 'Escape') {
+                    instance.close();
+                    document.removeEventListener("keydown", closeModal);
+                }
+            }
+            document.addEventListener("keydown", closeModal);
+            break;
+        }
+    };   
  }
-
-
