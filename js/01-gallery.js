cd5aa76c1d6b31361089c81bf32813ca42879db7
VolZa <vvolza@gmail.com>
2023-02-17 21:05:09 +0200

    hw07 version for handing in homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,18 +5,12 @@ const galleryEl = document.querySelector('.gallery');
 
 let creatGalleryPreview = ""; 
 
-
-// for (let item of galleryItems) {
-//     creatGalleryPreview +=
-//     (`<li><img style="dissplay:block; object-fit: cover; height=100%;"src=${item.preview} alt="${item.description}" width="340" /></li>`)
-// }
-
 galleryItems.forEach(item => {
     creatGalleryPreview +=
     (`<div class="gallery__item">
         <a class="gallery__link" href="${item.original}">
-            <img class="gallery__image" src="${item.preview}" alt="${item.description}"
-            style="dissplay:block; object-fit: cover; height=100%;"src=${item.preview} alt="${item.description}" width="340" />
+            <img class="gallery__image" data-source = "${item.original}" src="${item.preview}"
+            alt="${item.description}" style="dissplay:block; object-fit: cover; height=100%;"src=${item.preview} alt="${item.description}" width="340" />
         </a>
     </div>`)
 });
@@ -27,20 +21,6 @@ galleryEl.insertAdjacentHTML('beforeend', creatGalleryPreview);
 // Add a listener to a gallery item
 galleryEl.addEventListener("click", vievFullImage);
 
-// galleryEl.style.display = 'flex';
-// galleryEl.style.flexWrap = 'wrap';
-// galleryEl.style.gap = '10px';
-// galleryEl.style.listStyle = 'none';
-// galleryEl.style.cursor = 'zoom-in';
-// galleryEl.style.cursor.borderColor = 'read';
-
-// refs.galleryEl.children[0].children[0].src
-// console.log(`refs.galleryListEl.style = ${refs.galleryEl.children[0].children[0].src}`);
-
-
-
-
-
 function vievFullImage(ev) {
     // Override the default behavior
     ev.preventDefault();
@@ -49,30 +29,18 @@ function vievFullImage(ev) {
     if (ev.target.nodeName !== "IMG") {
         return;
     }
-    
 
+    const imgSelected = ev.target.getAttribute("data-source");
 
+    const instance = basicLightbox.create(`<img src="${imgSelected}" width="1280">`);
+    instance.show();
 
-    // get the value of the alt attribute  for the clicked element 
-    let altValue = ev.target.getAttribute("alt");
-
-    for (const image of galleryItems) {
-        if (image.description === altValue) {
-            const bigImg = image.original;               
-            
-            const instance = basicLightbox.create(`
-            <img src="${bigImg}" width="1280">
-            `);
-
-            instance.show();
-            function closeModal(ev) {
-                if (ev.code === 'Escape') {
-                    instance.close();
-                    document.removeEventListener("keydown", closeModal);
-                }
-            }
-            document.addEventListener("keydown", closeModal);
-            break;
+    //Close the modal window with the ESC key
+    function closeModal(ev) {
+        if (ev.code === 'Escape') {
+            instance.close();
+            document.removeEventListener("keydown", closeModal);
         }
-    };   
+    }
+    document.addEventListener("keydown", closeModal);  
  }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+let creatGalleryPreview = ""; 
+galleryItems.forEach(item => {
+    creatGalleryPreview +=
+    (`<a class="gallery__item" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}" title="${item.description}"
+            style="dissplay:block; object-fit: cover; height=100%;"src=${item.preview} alt="${item.description}" width="340" />
+        </a>
+   `)
+});
+// console.log(galleryItems);
+
+// Add code to DOM here
+galleryEl.insertAdjacentHTML('beforeend', creatGalleryPreview);
+
+// Create an object for viewing full-size images with a 250ms delay option
+new SimpleLightbox('.gallery a', { captionDelay: 250, });
